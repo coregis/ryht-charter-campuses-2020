b065ca55d767dc72da26377527736cea2cd46f0d
Matt Stevenson <matt@coregis.net>
2020-12-10 09:58:33 -0800

    Replacing campus points Mapbox tileset
    Forgot to calculate CPETECOP (percentage of economically disadvantaged students) for the newly added 2019 data, so all points were displaying as zero percent

--- scripts/onload.js
+++ scripts/onload.js
@@ -28,7 +28,7 @@ var loadedPolygonLayers = [];
 map.on('load', function() {
 	map.addSource('points',{
 		type:'vector',
-		url:'mapbox://core-gis.63frwn30'
+		url:'mapbox://core-gis.d6xp0h8m'
 	});
 
 	//add point data from Mapbox
@@ -36,7 +36,7 @@ map.on('load', function() {
 		'id': 'campuses',
 		'type': 'circle',
 		'source':'points',
-		'source-layer':'charter_campuses_12_09_20_geo-0hp0tr',
+		'source-layer':'charter_campuses_12_09_20_geo-06l6xt',
 		'filter':['==', ['number', ['get', 'year']], 1997],
 		'layout':{	},
 		'paint': {
